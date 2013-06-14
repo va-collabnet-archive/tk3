d894b4d00aec6698f945b176a8f1e35f6b90f081
kec <kec>
2013-06-14 03:43:00 +0000

    Added simple actions for testing.

--- ttk-service-layer/ttk-action-service/src/main/java/org/ihtsdo/ttk/services/action/AbstractTtkAction.java
+++ ttk-service-layer/ttk-action-service/src/main/java/org/ihtsdo/ttk/services/action/AbstractTtkAction.java
@@ -1,7 +1,5 @@
 package org.ihtsdo.ttk.services.action;
 
-import javafx.scene.Node;
-
 
 public abstract class AbstractTtkAction implements ActionBI {
 	private String label;
@@ -10,34 +8,23 @@ public abstract class AbstractTtkAction implements ActionBI {
 	private ThreadForAction threadForAction = ThreadForAction.BACKGROUND_THREAD;
 	private ExecutionState executionState = ExecutionState.READY;
 	
-	private Node graphic16x16;
-	private Node graphic24x24;
-	private Node graphic32x32;
-	private Node graphic128x128;
-	private Node graphic512x512;
-	
 	public AbstractTtkAction(String label, String description) {
 		super();
 		this.label = label;
 		this.description = description;
 	}
 
+        @Override
 	public String getLabel() {
 		return label;
 	}
 
-	public void setLabel(String label) {
-		this.label = label;
-	}
-
+        @Override
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
+        @Override
 	public ExecutionCriterion getExecutionCriterion() {
 		return executionCriterion;
 	}
@@ -46,6 +33,7 @@ public abstract class AbstractTtkAction implements ActionBI {
 		this.executionCriterion = executionCriterion;
 	}
 
+        @Override
 	public ThreadForAction getThreadForAction() {
 		return threadForAction;
 	}
@@ -54,52 +42,13 @@ public abstract class AbstractTtkAction implements ActionBI {
 		this.threadForAction = threadForAction;
 	}
 
+        @Override
 	public ExecutionState getExecutionState() {
 		return executionState;
 	}
 
 	public void setExecutionState(ExecutionState executionState) {
 		this.executionState = executionState;
-	}
-
-	public Node getGraphic16x16() {
-		return graphic16x16;
-	}
-
-	public void setGraphic16x16(Node graphic16x16) {
-		this.graphic16x16 = graphic16x16;
-	}
-
-	public Node getGraphic24x24() {
-		return graphic24x24;
-	}
-
-	public void setGraphic24x24(Node graphic24x24) {
-		this.graphic24x24 = graphic24x24;
-	}
-
-	public Node getGraphic32x32() {
-		return graphic32x32;
-	}
-
-	public void setGraphic32x32(Node graphic32x32) {
-		this.graphic32x32 = graphic32x32;
-	}
-
-	public Node getGraphic128x128() {
-		return graphic128x128;
-	}
-
-	public void setGraphic128x128(Node graphic128x128) {
-		this.graphic128x128 = graphic128x128;
-	}
-
-	public Node getGraphic512x512() {
-		return graphic512x512;
-	}
-
-	public void setGraphic512x512(Node graphic512x512) {
-		this.graphic512x512 = graphic512x512;
 	}
 
 }
