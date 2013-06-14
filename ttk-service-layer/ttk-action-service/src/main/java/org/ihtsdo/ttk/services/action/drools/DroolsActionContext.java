1e76ed08c4c3cdf47f1102ac250509aa07a1b30e
kec <kec>
2013-06-14 04:52:06 +0000

    Added simple actions for testing.

--- ttk-service-layer/ttk-action-service/src/main/java/org/ihtsdo/ttk/services/action/drools/DroolsActionContext.java
+++ ttk-service-layer/ttk-action-service/src/main/java/org/ihtsdo/ttk/services/action/drools/DroolsActionContext.java
@@ -1,6 +1,6 @@
 package org.ihtsdo.ttk.services.action.drools;
 
-import java.util.ArrayList;
+//~--- non-JDK imports --------------------------------------------------------
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,18 +8,54 @@ import javafx.collections.ObservableList;
 import org.ihtsdo.ttk.services.action.ActionBI;
 import org.ihtsdo.ttk.services.action.ActionContextBI;
 
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.ArrayList;
+
+/**
+ * Class description
+ *
+ *
+ * @version        Enter version here..., 13/06/13
+ * @author         Enter your name here...    
+ */
 public class DroolsActionContext {
 
-	protected ActionContextBI actionContext;
-	protected ObservableList<ActionBI> contextualActions = FXCollections.observableList(new ArrayList<ActionBI>());
+   /** Field description */
+   protected ObservableList<ActionBI> contextualActions =
+      FXCollections.observableList(new ArrayList<ActionBI>());
 
-	public DroolsActionContext(ActionContextBI actionContext) {
-		super();
-		this.actionContext = actionContext;
-	}
+   /** Field description */
+   protected ActionContextBI actionContext;
 
-	public ActionContextBI getActionContext() {
-		return actionContext;
-	}
+   /**
+    * Constructs ...
+    *
+    *
+    * @param actionContext
+    */
+   public DroolsActionContext(ActionContextBI actionContext) {
+      super();
+      this.actionContext = actionContext;
+   }
 
+   /**
+    * Method description
+    *
+    *
+    * @return
+    */
+   public ActionContextBI getActionContext() {
+      return actionContext;
+   }
+
+   /**
+    * Method description
+    *
+    *
+    * @return
+    */
+   public ObservableList<ActionBI> getContextualActions() {
+      return contextualActions;
+   }
 }
